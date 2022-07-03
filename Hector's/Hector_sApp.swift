1cd983f664d365078a9741ebc5bd48a6065af721
Joseph Vitale <joey.vitale31@gmail.com>
2022-07-03 13:53:56 +0300

    Initial Commit

---
+++ Hector's/Hector_sApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hector_sApp.swift
+//  Hector's
+//
+//  Created by Joey Vitale on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Hector_sApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hector'sUITests/Hector_sUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hector_sUITestsLaunchTests.swift
+//  Hector'sUITests
+//
+//  Created by Joey Vitale on 7/3/22.
+//
+
+import XCTest
+
+class Hector_sUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
