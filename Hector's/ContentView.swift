c512bfcfd6763d551c35fded08b93a021f614024
Joseph Vitale <joey.vitale31@gmail.com>
2022-07-05 16:02:56 +0300

    Updated welcome screen

--- Hector's/ContentView.swift
+++ Hector's/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("guns")
+                .clipShape(Circle())
+                .overlay {
+            Circle().stroke(.black, lineWidth: 6)
+                        }
+                .shadow(radius: 6)
+            VStack(alignment: .leading){
+                Text("Hector's")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Text("Est. 2016")
+                    .font(.subheadline)
+                Text("Latsia, Cyprus")
+                    .font(.subheadline)
+            }
+            .padding([.top, .leading, .bottom])
+            .position(x: 200, y: 40)
+            }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
+
+
